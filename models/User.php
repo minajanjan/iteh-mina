0ab7cdeeca8fc10b5d71b3c8833fa2047a3990ea
minajanjan <mincibakcup7@gmail.com>
2023-05-25 01:45:40 +0100

    user model

---
+++ models/User.php
@@ -0,0 +1,43 @@
+<?php
+
+class User
+{
+    public $id;
+    public $email;
+    public $username;
+    public $password;
+    public $nationality;
+
+    public function __construct(
+        $id,
+        $email,
+        $username,
+        $password,
+        $nationality
+    ) {
+        $this->id = $id;
+        $this->email = $email;
+        $this->username = $username;
+        $this->password = $password;
+        $this->nationality = $nationality;
+    }
+
+    public function createUser()
+    {
+        $host = 'localhost';
+        $user = 'admin';
+        $password = 'admin';
+        $database = 'restaurants';
+        $conn = mysqli_connect($host, $user, $password, $database);
+
+        $query = "INSERT INTO user(email, username, password, nationality) 
+            VALUES('$this->email', '$this->username', '$this->password',
+            '$this->nationality')";
+
+        if (mysqli_query($conn, $query)) {
+            header('Location: login.php');
+        } else {
+            echo 'Error: ' . mysqli_error($conn);
+        }
+    }
+}
